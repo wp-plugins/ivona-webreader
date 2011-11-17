1ab960e1699fc5ff793064ad671cb5bcdfc95b7d
ivonawebreader <ivonawebreader@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-17 13:42:43 +0000

    Updated to version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/ivona-webreader/tags/1.2@464166 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ivona_configuration.php
@@ -0,0 +1,102 @@
+<?
+class IVONAWebreaderConfig{
+    static function getConfig(){
+        return array(
+            // IVONA Webreader customer ID
+            // To configure this variable please use IVONA WebReader section in your Wordpress Settings
+            'wr_userID'=>'',
+            
+            // ID representing TTS voice
+            // See ivona_voicelist.php for details
+            // To configure this variable please use IVONA WebReader section in your Wordpress Settings
+            'wr_voiceID'=>'14',
+            
+            // GUI Language
+            // To configure this variable please use IVONA WebReader section in your Wordpress Settings 
+            'wr_lang'=>'en',
+            
+            // Background color of the player
+            // Color values must be in format '#xxxxxx' where
+            // 'xxxxxx' is hexadecimal value of the color
+            'wr_bgColor'=>'#000000',
+            
+            // Controls color of the player
+            // Color values must be in format '#xxxxxx' where
+            // 'xxxxxx' is hexadecimal value of the color
+            'wr_btnColor'=>'#FFFFFF',
+            
+            // Border color of the player
+            // Color values must be in format '#xxxxxx' where
+            // 'xxxxxx' is hexadecimal value of the color
+            'wr_borColor'=>'#666666',
+            
+            // Appearance of the player
+            // Allowed values are 0 (minified), 1 (text button) and 2 (full size)
+            'wr_playerMode'=>'1',
+            
+            // Alignment of the player
+            // Allowed values are 0 (left) and 1 (right)
+            'wr_scrollMode'=>'0', 
+            
+            // Shadow of the player
+            // Allowed values are 0 (disabled ) and 1 (enabled)
+            'wr_shadow'=>'1',
+            
+            // Alpha channel of the player
+            // Value must be an unsigned integer between 20 and 100 
+            'wr_alpha'=>'100',
+            
+            // Autoembed - forces embedding flash instead of HTML button
+            // Allowed values are 0 (disabled) or 1 (enabled)
+            'wr_autoembed'=>'0',
+            
+            // Speech synthesis volume
+            // Value must be an unsigned integer between 1 and 100
+            'wr_speechVolume'=>'100',
+            
+            // Height of the player
+            // Player's width is calculated from this value 
+            'wr_playerHeight'=>'26',
+            
+            // Speech synthesis rate
+            // Allowed values are 50 75 100 125 150 175 and 200
+            'wr_speechRate'=>'120',
+            
+            // Skip HTML elements with id attribute 
+            // You can specify multiple values separated by comma.
+            'wr_skip_id'=>'',                                         
+            
+            // Skip HTML elements with class attribute
+            // You can specify multiple values separated by comma. 
+            'wr_skip_class'=>'',  
+            
+            // Position of the player
+            // Currently only two values are possible:
+            // 'top' - displays player above the article
+            // 'bottom' - displays the player below the article
+            'wr_position'=>'top',       
+            
+             // Margin top (CSS Property)
+             // Margin above the each player
+            'wr_marginTop'=>'', 
+            
+            // Margin bottom  (CSS Property)
+            // Margin below each player
+            'wr_marginBottom'=>'10px',
+            
+            // Excluded IDS
+            // Articles, Post or pages to exclude from reading
+            // You can specify multiple IDs sparated by comma
+            // For exaple '1,42,834'
+            'wr_excludeIDS'=>'',
+            
+            // Embed hidden link to iWebReader website on your blog
+            // The link will only be visible to web robots 
+            'wr_hiddenLink'=>'yes',
+            
+            // Read modification date
+            // Should iWebReader read modification date of your posts?
+            'wr_readModDate'=>'yes',
+        ); 
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== iWebReader ===
+Contributors: ivonawebreader
+Donate link: http://www.iwebreader.com/
+Tags: page, tts, voiceover, text, speech, accessibility, post, sound
+Requires at least: 2.0
+Tested up to: 3.2.1
+Stable tag: 1.2
+
+
+This plugin will add iWebReader player to each post or page on your Wordpress blog. 
+iWebReader plugin requires a vaild account on iWebReader website. 
+
+== Description ==
+
+iWebReader is a high-tech service based on multiple-award-winning IVONA Text-To-Speech technology which allows website owners to voice-over web page content, making it more attractive and accessible to visitors. Thanks to this service, users can not only listen to selected text, but also download and save it as audio files. With iWebReader, you get access to all the voices and languages available in the IVONA suite of Internet services. The intuitive user interface and amazingly natural voices intensify the visitor’s experience of your website, while enhancing your brand. 
+
+== Installation ==
+1. Activate your service on [iWebReader](http://www.iwebreader.com/) website.
+1. Define list of your domains on [security page](https://secure.iwebreader.com/account/security.php).
+1. Upload plugin files to the `/wp-content/plugins/` 
+1. Activate the plugin through the `Plugins` menu in WordPress
+1. Select the `Settings -> IVONA Webreader` and enter your service ID.
+1. Choose one of all available IVONA voices and save your configuration.
+
+== Frequently Asked Questions ==
+
+You can find full list of Frequently Asked Questions on [iWebReader](http://www.iwebreader.com/ "iWebReader") website.
+
+If you have experienced any problems with this plugin please let us know by contacting our support department at [iWebReader](http://www.iwebreader.com/ "iWebReader") website.
+
+== Screenshots ==
+
+1. iWebReader player on default Wordpress instance.
+2. iWebReader configuration on Site admin pages
+
+== Changelog ==
+
+= 1.2 =
+* Added new Australian English voice - Nicole.
+
+= 1.1 =
+* Improved overall stability of the plugin.
+* Updated voices list with new TTS voices.
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.2 =
+* Added new Australian English voice - Nicole.
+
+= 1.1 =
+* Added new confuration options.
+* Improved stability of the plugin.
+* Added support for new TTS voices 
+
+= 1.0 =
+* This is initial release of the plugin
